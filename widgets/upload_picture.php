fe582b73365b3a9730bc165d12b18de89a98354e
sidewindernet <sidewindernet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-14 22:03:04 +0000

    .
    git-svn-id: http://svn.wp-plugins.org/picasso/trunk@397203 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- widgets/upload_picture.php
+++ widgets/upload_picture.php
@@ -10,9 +10,8 @@
 -->
 
 <div id="swfupload-control">
-	<input type="button" id="flash-browse-button" />
+	<input type="button" id="picasso-upload-button" />
 	<div id="media-items" class="hide-if-no-js"> </div>
-	<ol id="log"></ol>
 </div>
 <?php
 	$siteurl = get_site_url() . "/wp-includes/js/swfupload/swfupload.swf";
@@ -30,33 +29,30 @@ SWFUpload.onload = function() {
 			button_width: "132",
 			button_text_top_padding: 3,
 			button_image_url: "<?php echo $button; ?>",
-			button_placeholder_id: "flash-browse-button",
+			button_placeholder_id: "picasso-upload-button",
 			upload_url : "admin-post.php",
 			flash_url : "<?php echo $siteurl; ?>",
 			file_post_name: "file",
-			file_types: "*.*",
+			file_types: "*.jpg;*.png;*.gif",
 			post_params : {
 				"action" : "picasso-upload-picture",
 				"aid" : <?php echo $_GET['id']; ?>,
-				"album" : "<?php echo $album; ?>",
-				"post_id" : "0",
-				"auth_cookie" : "admin|1294697893|d39c7e8ea40c2bd262ef4d8e39d8c55c",
-				"logged_in_cookie": "admin|1294697893|a100aa1736c4b529eb9c5167a00e1809",
-				"_wpnonce" : "3ef316d551",
-				"type" : "",
-				"tab" : "",
-				"short" : "1"
+				"album" : "<?php echo $album; ?>"
 			},
 			file_size_limit : "2097152b",
+			//swfupload_loaded_handler : swfuploadHandler,
+			
 			file_dialog_start_handler : fileDialogStart,
 			file_queued_handler : fileQueued,
+			//file_queue_error_handler : fileQueueError,
+			file_dialog_complete_handler : fileDialogComplete,
+			
 			upload_start_handler : uploadStart,
 			upload_progress_handler : uploadProgress,
-			upload_error_handler : uploadError,
 			upload_success_handler : uploadSuccess,
-			upload_complete_handler : uploadComplete,
-			file_queue_error_handler : fileQueueError,
-			file_dialog_complete_handler : fileDialogComplete,
+			//upload_error_handler : uploadError,
+			//upload_complete_handler : uploadComplete,
+			
 			custom_settings : {
 				degraded_element_id : "html-upload-ui", // id of the element displayed when swfupload is unavailable
 				swfupload_element_id : "flash-upload-ui" // id of the element displayed when swfupload is available
@@ -66,6 +62,85 @@ SWFUpload.onload = function() {
 		swfu = new SWFUpload(settings);
 };
 
-
+function fileDialogStart() {
+    jQuery("#media-upload-error").empty()
+}
+function fileQueued(fileObj) {
+    jQuery(".media-blank").remove();
+    if (jQuery("form.type-form #media-items").children().length == 1 && jQuery(".hidden", "#media-items").length > 0) {
+        jQuery(".describe-toggle-on").show();
+        jQuery(".describe-toggle-off").hide();
+        jQuery(".slidetoggle").slideUp(200).siblings().removeClass("hidden")
+    }
+    jQuery("#media-items").append('<div id="media-item-' + fileObj.id + '" class="media-item child-of-' + post_id + '"><div class="progress"><div class="bar"></div></div><div class="filename original"><span class="percent"></span> ' + fileObj.name + "</div></div>");
+    jQuery(".progress", "#media-item-" + fileObj.id).show();
+    jQuery("#insert-gallery").attr("disabled", "disabled");
+    jQuery("#cancel-upload").attr("disabled", "")
+}
+function uploadStart(fileObj) {
+    try {
+        if (typeof topWin.tb_remove != "undefined") {
+            topWin.jQuery("#TB_overlay").unbind("click", topWin.tb_remove)
+        }
+    } catch(e) {}
+    return true
+}
+function uploadProgress(fileObj, bytesDone, bytesTotal) {
+    var w = jQuery("#media-items").width() - 2,
+    item = jQuery("#media-item-" + fileObj.id);
+    jQuery(".bar", item).width(w * bytesDone / bytesTotal);
+    jQuery(".percent", item).html(Math.ceil(bytesDone / bytesTotal * 100) + "%");
+    if (bytesDone == bytesTotal) {
+        //jQuery(".bar", item).html('<strong class="crunching">' + swfuploadL10n.crunching + "</strong>")
+    }
+}
+function uploadSuccess(fileObj, serverData) {
+    if (serverData.match("media-upload-error")) {
+        jQuery("#media-item-" + fileObj.id).html(serverData);
+        return
+    }
+    //prepareMediaItem(fileObj, serverData);
+    //updateMediaForm();
+    if (jQuery("#media-item-" + fileObj.id).hasClass("child-of-" + post_id)) {
+        jQuery("#attachments-count").text(1 * jQuery("#attachments-count").text() + 1)
+    }
+}
+function uploadComplete(fileObj) {
+    if (swfu.getStats().files_queued == 0) {
+        jQuery("#cancel-upload").attr("disabled", "disabled");
+        jQuery("#insert-gallery").attr("disabled", "")
+    }
+}
+function fileQueueError(fileObj, error_code, message) {
+    if (error_code == SWFUpload.QUEUE_ERROR.QUEUE_LIMIT_EXCEEDED) {
+        wpQueueError(swfuploadL10n.queue_limit_exceeded)
+    } else {
+        if (error_code == SWFUpload.QUEUE_ERROR.FILE_EXCEEDS_SIZE_LIMIT) {
+            fileQueued(fileObj);
+            wpFileError(fileObj, swfuploadL10n.file_exceeds_size_limit)
+        } else {
+            if (error_code == SWFUpload.QUEUE_ERROR.ZERO_BYTE_FILE) {
+                fileQueued(fileObj);
+                wpFileError(fileObj, swfuploadL10n.zero_byte_file)
+            } else {
+                if (error_code == SWFUpload.QUEUE_ERROR.INVALID_FILETYPE) {
+                    fileQueued(fileObj);
+                    wpFileError(fileObj, swfuploadL10n.invalid_filetype)
+                } else {
+                    wpQueueError(swfuploadL10n.default_error)
+                }
+            }
+        }
+    }
+}
+function fileDialogComplete(num_files_queued) {
+    try {
+        if (num_files_queued > 0) {
+            this.startUpload()
+	        } 
+    } catch(ex) {
+        this.debug(ex)
+    }
+}
 //]]>
 </script> 
