1d44af313c7c2163e9305aff2de17b03a0c97a3d
sidewindernet <sidewindernet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-14 22:06:32 +0000

    fixed url bug
    git-svn-id: http://svn.wp-plugins.org/picasso/trunk@397218 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/gallery.php
+++ views/gallery.php
@@ -5,8 +5,16 @@
 		$info = pathinfo($album->filename);
 		$t = $info['filename'] . "_thumb." . $info['extension'];
 		$s = md5($album->name);
+    
+    $query = explode('?', $_SERVER['REQUEST_URI']);
+    $add = '?';
+    if (isset($query[1])) {
+      $add .= $query[1].'&amp;';
+    }
+    $add .= "id={$album->aid}";
+    $url = $query[0].$add;
 
-	  echo "<a href='{$_SERVER["REQUEST_URI"]}&id={$album->aid}'>";
+	  echo "<a href='$url'>";
 	  echo "<img class='thumb' src='" . PICASSO_UPLOAD_URL . "/$s/$t' /> ";
 	  echo "</a>";
 
