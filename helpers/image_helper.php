43f8e524b9225b9a36990c0ba8ed2261a5ad7d97
sidewindernet <sidewindernet@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-14 22:09:20 +0000

    fixed aspect ratio issue
    git-svn-id: http://svn.wp-plugins.org/picasso/trunk@397227 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- helpers/image_helper.php
+++ helpers/image_helper.php
@@ -13,6 +13,19 @@ class ImageHelper
     list($width, $height, $type) = getimagesize($image);
     $mime = image_type_to_mime_type($type);
 		
+		//Preserve aspect ratio
+
+		$wRatio = $w / $width;
+		$hRatio = $h / $height;
+		
+		if(($wRatio * $height) < $w) {
+			$h = ceil($wRatio * $height);
+		} 
+		else {
+			$w = ceil($hRatio * $width);
+		}
+		
+		
     $thumb = imagecreatetruecolor($w, $h);
     switch($mime) {
     	case 'image/jpeg':    
@@ -38,6 +51,69 @@ class ImageHelper
     $t = $info['filename'] . "_thumb." . $info['extension'];
     
     $image = PICASSO_UPLOAD_DIR . "/$album/$t";
+    
+    
+    switch($mime) {
+    	case 'image/jpeg':
+    		imagejpeg($thumb, $image, 100);
+    		break;
+    		
+  		case 'image/gif':
+    		imagegif($thumb, $image);
+  			break;
+  			
+			case 'image/png':
+    		imagepng($thumb, $image, 100);
+  			break;
+  			
+    	default:
+    		return false;
+    }
+    
+    return true;
+    		
+  }
+	
+  function resize($filename, $album, $w = 800, $h = 800)
+  {
+  	$image = PICASSO_UPLOAD_DIR . "/$album/$filename";
+  	
+    list($width, $height, $type) = getimagesize($image);
+    $mime = image_type_to_mime_type($type);
+		
+		//Preserve aspect ratio
+		$wRatio = $w / $width;
+		$hRatio = $h / $height;
+		
+		if(($wRatio * $height) < $w) {
+			$h = ceil($wRatio * $height);
+		} 
+		else {
+			$w = ceil($hRatio * $width);
+		}
+		
+		
+    $thumb = imagecreatetruecolor($w, $h);
+    switch($mime) {
+    	case 'image/jpeg':    
+    		$temp = imagecreatefromjpeg($image);
+    		break;
+    		
+  		case 'image/gif':
+    		$temp = imagecreatefromgif($image);
+  			break;
+  			
+			case 'image/png':
+    		$temp = imagecreatefrompng($image);
+  			break;
+  			
+    	default:
+    		return false;
+    }
+    
+    imagecopyresampled($thumb, $temp, 0, 0, 0, 0, $w, $h, $width, $height);	
+    
+    $image = PICASSO_UPLOAD_DIR . "/$album/$filename";
     
     
     switch($mime) {
